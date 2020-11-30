de31167e49c7893c0ac7b6f02d19e270e5ba7ef0
DESKTOP-7BIICIT\stask <b3l3nk4ss@gmail.com>
2020-11-30 15:28:57 +0200

    Backend Database setup added products user data structure mangoose scheme

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -71,12 +70,6 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
   color: '#009AE1',
-}
-
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
 }
 
 export default Rating
